7c4719d8160cc1b80e2fbe7e66768c5cafb31a24
Christian Lomeli <christian.lomeli2591@alumnos.udg.mx>
2023-06-07 15:41:40 -0600

    If else y elif

---
+++ 4.2-ifelse.py
@@ -0,0 +1,7 @@
+dia = input("Ingresa el dia de la semana")
+
+if dia.lower() == "lunes" or dia.lower == "miercoles" or dia.lower() == "viernes":
+    print("Toca curso")
+else:
+    print("Toca descansar")
+print("Adios")
